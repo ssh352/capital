19f0f0682ed347c7e2379edeb5a7c2dbb91be374
coreyluo <34770843+coreyluo@users.noreply.github.com>
2018-12-21 00:29:56 +0800

    Update init.sql
    普通行情建表

--- test/src/test/resources/sql/init.sql
+++ test/src/test/resources/sql/init.sql
@@ -30,3 +30,38 @@ CREATE TABLE `tickey_info` (
   PRIMARY KEY (`id`),
   KEY `idx_create_time` (`create_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='股票信息';
+
+CREATE TABLE `depth_marcket_data` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `ask` varchar(100) NOT NULL COMMENT '10档申卖价',
+  `ask_qty` int(10) NOT NULL COMMENT '十档申卖量',
+  `avg_price` decimal(10,2) NOT NULL COMMENT '当日均价=(turnover/qty)',
+  `bid` varchar(100) DEFAULT NULL COMMENT '十档申买价',
+	`bid_qty` int(10) NOT NULL COMMENT '十档申买量',
+  `close_price`decimal(10,2) NOT NULL COMMENT '今收盘',
+  `curr_delta` decimal(10,2) NOT NULL COMMENT '预留',
+	`dataTime` TIMESTAMP COMMENT '行情时间',
+  `exchange_type` TINYINT(2) NOT NULL COMMENT '交易所类型 上海 1， 深圳 2， 未知 3',
+  `high_price` decimal(10,2) NOT NULL COMMENT '最高价',
+  `last_price` decimal(10,2) NOT NULL COMMENT '最新价',
+  `lower_limit_price` decimal(10,2) NOT NULL COMMENT '跌停价',
+  `low_price` decimal(10,2) NOT NULL COMMENT '最低价',
+  `open_price` decimal(10,2) NOT NULL COMMENT '今日开盘价',
+	`pre_close_price` decimal(10,2) NOT NULL COMMENT '昨日收盘价',
+	`pre_delta` decimal(10,2) NOT NULL COMMENT '昨日预留',
+ 	`pre_settlement_price` decimal(10,2) NOT NULL COMMENT '昨日结算价',
+	`pre_total_long_positon` bigint(10)   COMMENT '昨日持仓量(张)(目前未填写)',
+	`qty` bigint(10)   COMMENT '数量，为总成交量（单位股，与交易所一致）',
+	`settlement_price` decimal(10,2) NOT NULL COMMENT '今日结算价',
+	`ticker` varchar(100) NOT NULL COMMENT '股票代码',
+	`ticker_status` varchar(100) NOT NULL COMMENT '当前交易状态说明',
+ 	`total_long_positon` bigint(10)   COMMENT '持仓量(张)',
+	`trades_count` bigint(10)   COMMENT '成交笔数',
+	`turnover` decimal(10,2) NOT NULL COMMENT '成交金额，为总成交金额（单位元，与交易所一致）',
+	`upper_limit_price` decimal(10,2) NOT NULL COMMENT '涨停价',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='普通行情';
+
+
