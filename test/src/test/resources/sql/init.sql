903a8999322dd365aee623613e0f1f4984d7e19e
coreyluo <34770843+coreyluo@users.noreply.github.com>
2018-12-20 23:31:35 +0800

    Update init.sql
    股票信息建表语句

--- test/src/test/resources/sql/init.sql
+++ test/src/test/resources/sql/init.sql
@@ -14,3 +14,19 @@ CREATE TABLE `user_info` (
   UNIQUE KEY `uk_id_card` (`id_card`),
   KEY `idx_name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COMMENT='用户信息';
+
+CREATE TABLE `tickey_info` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `ticker` varchar(30) NOT NULL COMMENT '股票代码',
+  `ticket_name` varchar(60) NOT NULL COMMENT '股票名称',
+  `exchange_type` tinyint(2) NOT NULL COMMENT '交易所类型 上海 1， 深圳 2， 未知 3',
+  `ticker_type` tinyint(2) DEFAULT NULL COMMENT '合约类型  1 股票 2 其他',
+  `buy_qty_unit` int(8) NOT NULL COMMENT '合约最小交易量',
+  `upper_limit_price` decimal(10,0) NOT NULL COMMENT '涨停板价',
+  `lower_limit_price` decimal(10,0) NOT NULL COMMENT '跌停板价',
+  `pre_close_price` decimal(10,0) NOT NULL COMMENT '昨收盘价',
+  `price_tick` decimal(10,0) NOT NULL COMMENT '最小变动价位',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='股票信息';
