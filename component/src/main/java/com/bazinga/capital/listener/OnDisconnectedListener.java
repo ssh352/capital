b33a0d4f92992eb9378ac5fbe97a828d3605120f
yunshan <lxw5347@163.com>
2019-01-08 00:35:59 +0800

    for test

--- component/src/main/java/com/bazinga/capital/listener/OnDisconnectedListener.java
+++ component/src/main/java/com/bazinga/capital/listener/OnDisconnectedListener.java
@@ -2,8 +2,10 @@ package com.bazinga.capital.listener;
 
 import com.bazinga.capital.api.QuoteApiService;
 import com.bazinga.capital.api.TradeApiService;
+import com.bazinga.capital.constant.LoginState;
 import com.bazinga.capital.enums.LoginTypeEnum;
 import com.bazinga.capital.event.OnDisconnectedEvent;
+import com.zts.xtp.common.enums.ExchangeType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -28,6 +30,10 @@ public class OnDisconnectedListener implements ApplicationListener<OnDisconnecte
         if (LoginTypeEnum.QUOTE.getCode() == onDisconnectedEvent.getLoginType()) {
             log.info("监听到行情连接中断 重新连接");
             quoteApiService.connectAndLogin();
+            if(LoginState.LOGIN_RESULT){
+                log.info("重新订阅深圳行情");
+                quoteApiService.subscribeAllMarketData(ExchangeType.SZ.getType());
+            }
         } else if (LoginTypeEnum.TRADE.getCode() == onDisconnectedEvent.getLoginType()) {
             log.info("监听到交易连接中断 重新连接");
             tradeApiService.initAndLogin();
